26dd884daabcb482aca5c9cf68ba981fd5dc1f9b
Ivan <clicker.heroes.acg@gmail.com>
2022-11-29 15:05:31 +0300

    tests improved

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -8,10 +8,60 @@ use std::fs::File;
 use std::io::Write;
 use tempfile::TempDir;
 
+const SFC: &str = "safe-mac-cli";
+
 #[test]
-fn basic_case() -> Result<()> {
+fn basic_case_success() -> Result<()> {
     let tmp = TempDir::new()?;
-    File::create(tmp.path().join("config.txt"))?.write_all(
+    init_config_file(&tmp, "config.txt")?;
+    assert_cmd::Command::cargo_bin(SFC)
+        .unwrap()
+        .current_dir(tmp.path())
+        .arg("config.txt")
+        .assert()
+        .success();
+    check_result(tmp)?;
+    Ok(())
+}
+
+#[test]
+fn basic_case_fail() -> Result<()> {
+    let tmp = TempDir::new()?;
+    assert_cmd::Command::cargo_bin(SFC)
+        .unwrap()
+        .current_dir(tmp.path())
+        .arg("config.txt")
+        .assert()
+        .failure();
+    Ok(())
+}
+
+#[test]
+fn default_arg_success() -> Result<()> {
+    let tmp = TempDir::new()?;
+    init_config_file(&tmp, ".env")?;
+    assert_cmd::Command::cargo_bin(SFC)
+        .unwrap()
+        .current_dir(tmp.path())
+        .assert()
+        .success();
+    check_result(tmp)?;
+    Ok(())
+}
+
+#[test]
+fn default_arg_fail() -> Result<()> {
+    let tmp = TempDir::new()?;
+    assert_cmd::Command::cargo_bin(SFC)
+        .unwrap()
+        .current_dir(tmp.path())
+        .assert()
+        .failure();
+    Ok(())
+}
+
+fn init_config_file(tmp: &TempDir, name: &str) -> Result<()> {
+    File::create(tmp.path().join(name))?.write_all(
         "
             BOT_TOKEN=5952187433:AAElWDo96OZExms06d4zqKGvtJ81BI-DaXw
             BOT_PORT=3001
@@ -21,12 +71,10 @@ fn basic_case() -> Result<()> {
         "
         .as_bytes(),
     )?;
-    assert_cmd::Command::cargo_bin("safe-mac-cli")
-        .unwrap()
-        .current_dir(tmp.path())
-        .arg("config.txt")
-        .assert()
-        .success();
+    Ok(())
+}
+
+fn check_result(tmp: TempDir) -> Result<()> {
     match fs::read_to_string(tmp.path().join(".safe_env")) {
         Ok(line) => {
             assert_eq!(line.contains("MAC_ADDR"), true)
